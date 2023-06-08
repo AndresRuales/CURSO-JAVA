1a6e57857acfce45892c935e54f8ea040075640a
Andres Ruales <danielandresruales@hotmail.com>
2023-06-07 21:13:08 -0500

    Se ven las variables y tipos de datos, se hace un cast

---
+++ src/Ejemplo.java
@@ -0,0 +1,48 @@
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		
+		int edad = 28;
+		
+		System.out.println(edad);
+
+		edad = 44;
+		
+		System.out.println(edad);
+		
+		int suma = edad + 2;
+
+		System.out.println(suma);
+		
+		
+		//Tipo double
+		
+		double salario = 1250.5;
+		System.out.println(salario);
+		
+		//Hacer Cast
+		
+		int cambioAint = (int) salario;
+		
+		System.out.println(cambioAint);
+		
+		//Tipo Long, para tipos de datos grandes
+		long prueba = 1222222222222222222L;
+
+		//shot para un numero pequeño
+		
+		short numeroPequeno = 123;
+		//byte, el mas pequeno
+		
+		byte numeroAunMasPequeno = 1;
+		
+		//float, decimal pequno
+		
+		float decimal = 4.5f;
+		
+		
+	}
+	
+}
