aa4a1b2f54829e93a57c7aedd1bfab29487d4513
Andres Ruales <danielandresruales@hotmail.com>
2023-06-17 12:13:54 -0500

    Se comienza a realizar ejemplos de manejo de excepciones

--- src/ManejoExcepciones/Flujo.java
+++ src/ManejoExcepciones/Flujo.java
@@ -1,5 +1,25 @@
 package ManejoExcepciones;
 
 public class Flujo {
-
+	
+	//Pila de ejecucion
+	public static void main(String[] args) {
+		System.out.println("Inicio main");
+		metodo1();
+		System.out.println("Fin main");
+	}
+	
+	private static void metodo1() {
+		System.out.println("Inicion metodo1");
+		metodo2();
+		System.out.println("Fin metodo1");
+	}
+	
+	private static void metodo2() {
+		System.out.println("Inicion metodo2");
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+		System.out.println("fin metodo2");
+	}
 }
